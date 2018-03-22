03355425c90826c73dc9b527717c48b8c9ba0d61
Dan Leehr <dan.leehr@duke.edu>
2018-03-22 14:39:59 -0400

    Tidy up and update script to working

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,21 @@
 
 set -ex
 
-# oc new-project gcb-bespin
+# Create a project
+oc new-project gcb-bespin
+
+# Make sure we're using the right project
 oc project gcb-bespin
+
+# Create configs
 oc create -f bespin-db-config.yml
-oc create -f bespin-db.yml
 oc create -f bespin-api-config.yml
+oc create -f bespin-revproxy-config.yml
+
+# Now create the services
+oc create -f bespin-db.yml
 oc create -f bespin-api.yml
+oc create -f bespin-revproxy.yml
+
+# Finally, create the one route into the proxy
+oc create -f bespin-revproxy-route.yml
